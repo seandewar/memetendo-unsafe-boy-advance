59e34e64a40dc593b578ec060647fab8b33f9d51
Sean Dewar <seandewar@users.noreply.github.com>
2022-01-24 04:49:40 +0000

    feat(arm7tdmi/thumb): impl ADD, SUB (reg and immediate)

--- src/arm7tdmi/thumb.rs
+++ src/arm7tdmi/thumb.rs
@@ -90,11 +90,11 @@ impl Cpu {
                         // LSR, ASR
                         0b01 | 0b10 => {
                             self.reg.cpsr.carry = (val >> (offset - 1)) & 1 != 0;
-                            if op == 0b01 {
-                                self.reg.r[r_dst] = val >> offset;
+                            self.reg.r[r_dst] = if op == 0b01 {
+                                val >> offset
                             } else {
-                                self.reg.r[r_dst] = ((val as i32) >> offset) as _;
-                            }
+                                ((val as i32) >> offset) as _
+                            };
                         }
                         _ => unreachable!("format should be AddSub"),
                     }
@@ -102,7 +102,32 @@ impl Cpu {
 
                 self.reg.cpsr.set_zn_from(self.reg.r[r_dst]);
             }
-            AddSub => todo!(),
+            // TODO: 1S cycle
+            AddSub => {
+                let r_dst = instr as usize & 0b111;
+                let val1 = self.reg.r[(instr as usize >> 3) & 0b111];
+                let r_or_imm = (instr >> 6) & 0b111;
+                let op = (instr >> 9) & 0b11;
+
+                let mut val2 = if op & 0b10 == 0 {
+                    self.reg.r[r_or_imm as usize] // register
+                } else {
+                    r_or_imm as u32 // immediate
+                };
+                if op & 1 != 0 {
+                    val2 = -(val2 as i32) as _; // SUB
+                }
+
+                let result: u64 = (val1 as u64).wrapping_add(val2 as _);
+                let (val1_signed, val2_signed) = (val1 as i32, val2 as i32);
+                let (val1_neg, val2_neg) = (val1_signed.is_negative(), val2_signed.is_negative());
+                let same_sign = val1_neg == val2_neg;
+
+                self.reg.cpsr.overflow = same_sign && (result as i32).is_negative() != val1_neg;
+                self.reg.cpsr.carry = result > u32::MAX as _;
+                self.reg.cpsr.set_zn_from(result as _);
+                self.reg.r[r_dst] = result as _;
+            }
             MoveCmpAddSubImm => todo!(),
             AluOp => todo!(),
             HiRegOpBranchExchange => todo!(),
@@ -170,80 +195,162 @@ mod tests {
 
     #[test]
     fn execute_thumb_move_shifted_reg() {
-        // LSL R4,R1,#3
+        // LSL Rd,Rs,#Offset
         test_instr!(
             |cpu: &mut Cpu| cpu.reg.r[1] = 0b10,
-            0b000_00_00011_001_100,
+            0b000_00_00011_001_100, // LSL R4,R1,#3
             [0, 0b10, 0, 0, 0b10_000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         );
-        // LSL R0,R7,#15
         test_instr!(
             |cpu: &mut Cpu| cpu.reg.r[7] = 1,
-            0b000_00_01111_111_000,
+            0b000_00_01111_111_000, // LSL R0,R7,#15
             [1 << 15, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         );
-        // LSL R0,R7,#1
         test_instr!(
             |cpu: &mut Cpu| cpu.reg.r[7] = 1 << 31,
-            0b000_00_00001_111_000,
+            0b000_00_00001_111_000, // LSL R0,R7,#1
             [0, 0, 0, 0, 0, 0, 0, 1 << 31, 0, 0, 0, 0, 0, 0, 0, 0],
             carry | zero
         );
-        // LSL R0,R7,#10
         test_instr!(
-            0b000_00_01010_111_000,
+            0b000_00_01010_111_000, // LSL R0,R7,#10
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             zero
         );
-        // LSL R0,R0,#0
         test_instr!(
             |cpu: &mut Cpu| cpu.reg.r[0] = u32::MAX,
-            0b000_00_00000_000_000,
+            0b000_00_00000_000_000, // LSL R0,R0,#0
             [u32::MAX, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             negative
         );
 
-        // LSR R4,R1,#2
+        // LSR Rd,Rs,#Offset
         test_instr!(
             |cpu: &mut Cpu| cpu.reg.r[1] = 0b100,
-            0b000_01_00011_001_100,
+            0b000_01_00011_001_100, // LSR R4,R1,#2
             [0, 0b100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             zero | carry
         );
-        // LSR R4,R1,#2
         test_instr!(
             |cpu: &mut Cpu| cpu.reg.r[1] = 0b10,
-            0b000_01_00011_001_100,
+            0b000_01_00011_001_100, // LSR R4,R1,#2
             [0, 0b10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             zero
         );
-        // LSR R7,R7,#31
         test_instr!(
             |cpu: &mut Cpu| cpu.reg.r[7] = 1 << 31,
-            0b000_01_11111_111_111,
+            0b000_01_11111_111_111, // LSR R7,R7,#31
             [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         );
-        // LSR R7,R7,#0
         test_instr!(
             |cpu: &mut Cpu| cpu.reg.r[7] = 1 << 31,
-            0b000_01_00000_111_111,
+            0b000_01_00000_111_111, // LSR R7,R7,#0
             [0, 0, 0, 0, 0, 0, 0, 1 << 31, 0, 0, 0, 0, 0, 0, 0, 0],
             negative
         );
 
-        // ASR R7,R7,#31
+        // ASR Rd,Rs,#Offset
         test_instr!(
             |cpu: &mut Cpu| cpu.reg.r[7] = 1 << 31,
-            0b000_10_11111_111_111,
+            0b000_10_11111_111_111, // ASR R7,R7,#31
             [0, 0, 0, 0, 0, 0, 0, u32::MAX, 0, 0, 0, 0, 0, 0, 0, 0],
             negative
         );
-        // ASR R0,R5,#1
         #[rustfmt::skip]
         test_instr!(
             |cpu: &mut Cpu| cpu.reg.r[5] = !(1 << 31),
-            0b000_10_00001_101_000,
+            0b000_10_00001_101_000, // ASR R0,R5,#1
             [!(0b11 << 30), 0, 0, 0, 0, !(1 << 31), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            carry
+        );
+    }
+
+    #[test]
+    fn execute_thumb_add_sub() {
+        // ADD Rd,Rs,Rn
+        test_instr!(
+            |cpu: &mut Cpu| {
+                cpu.reg.r[1] = 13;
+                cpu.reg.r[7] = 7;
+            },
+            0b00011_00_111_001_100, // ADD R4,R1,R7
+            [0, 13, 0, 0, 20, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0],
+        );
+        test_instr!(
+            |cpu: &mut Cpu| {
+                cpu.reg.r[7] = 1;
+            },
+            0b00011_00_111_111_111, // ADD R7,R7,R7
+            [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+        );
+        test_instr!(
+            |cpu: &mut Cpu| {
+                cpu.reg.r[6] = u32::MAX;
+                cpu.reg.r[7] = 1;
+            },
+            0b00011_00_111_110_000, // ADD R0,R6,R7
+            [0, 0, 0, 0, 0, 0, u32::MAX, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            carry | zero
+        );
+        test_instr!(
+            |cpu: &mut Cpu| {
+                cpu.reg.r[0] = -5 as _;
+                cpu.reg.r[1] = -10 as _;
+            },
+            0b00011_00_000_001_010, // ADD R2,R1,R0
+            [-5 as _, -10 as _, -15 as _, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            negative | carry
+        );
+        #[rustfmt::skip]
+        test_instr!(
+            |cpu: &mut Cpu| {
+                cpu.reg.r[0] = i32::MIN as _;
+                cpu.reg.r[1] = -1 as _;
+            },
+            0b00011_00_000_001_010, // ADD R2,R1,R0
+            [i32::MIN as _, -1 as _, i32::MIN.wrapping_sub(1) as _, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            carry | overflow
+        );
+
+        // SUB Rd,Rs,Rn
+        #[rustfmt::skip]
+        test_instr!(
+            |cpu: &mut Cpu| {
+                cpu.reg.r[3] = i32::MIN as _;
+                cpu.reg.r[6] = i32::MAX as _;
+            },
+            0b00011_01_110_011_000, // SUB R0,R3,R6
+            [1, 0, 0, i32::MIN as _, 0, 0, i32::MAX as _, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            carry | overflow
+        );
+        test_instr!(
+            |cpu: &mut Cpu| cpu.reg.r[0] = -5 as _,
+            0b00011_01_000_000_010, // SUB R2,R0,R0
+            [-5 as _, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            carry | zero
+        );
+        test_instr!(
+            |cpu: &mut Cpu| {
+                cpu.reg.r[0] = 5;
+                cpu.reg.r[1] = -10 as _;
+            },
+            0b00011_01_000_001_010, // SUB R2,R1,R0
+            [5, -10 as _, -15 as _, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            negative | carry
+        );
+
+        // ADD Rd,Rs,#nn
+        test_instr!(
+            |cpu: &mut Cpu| cpu.reg.r[0] = 10,
+            0b00011_10_101_000_000, // ADD R0,R0,#5
+            [15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        );
+
+        // SUB Rd,Rs,#nn
+        test_instr!(
+            |cpu: &mut Cpu| cpu.reg.r[0] = 10,
+            0b00011_11_010_000_000, // SUB R0,R0,#2
+            [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             carry
         );
     }
