5488bbf6de0b83b3f6bc82271d46c5cee9f89548
Sean Dewar <seandewar@users.noreply.github.com>
2023-03-15 02:42:27 +0000

    feat(web-memetendo): impl frame skip
    The logic differs slightly from (sdl) memetendo, as we compare against the same
    timestamp given to request_animation_frame() after each skipped frame, which of
    course only reflects the time from before the first skipped frame; but it works
    OK.

--- web-memetendo/src/main.rs
+++ web-memetendo/src/main.rs
@@ -93,7 +93,9 @@ struct Context {
     document: Document,
     screen: Rc<RefCell<Screen>>, // TODO: Rc<RefCell<>> sucks; is there a good way around it?
     gba: Option<Gba>,
-    updater: Option<Closure<dyn Fn()>>,
+    updater: Option<Closure<dyn Fn(f64)>>,
+    max_frame_skip: u32,
+    next_frame_ms: Option<f64>,
     selected_bios_rom: Option<bios::Rom>,
     selected_cart_rom: Option<cart::Rom>,
 }
@@ -106,6 +108,8 @@ impl Context {
             screen: Rc::new(RefCell::new(Screen::new(window)?)),
             gba: None,
             updater: None,
+            max_frame_skip: 3,
+            next_frame_ms: None,
             selected_bios_rom: None,
             selected_cart_rom: None,
         })
@@ -127,27 +131,48 @@ fn maybe_start_emulation(ctx: &Rc<RefCell<Context>>) -> bool {
         bios_rom.clone(),
         Cartridge::new(cart_rom.clone(), backup_type),
     ));
-
     borrowed_ctx.screen.borrow().clear();
+    borrowed_ctx.next_frame_ms = None;
 
     if borrowed_ctx.updater.is_none() {
         borrowed_ctx.updater = Some({
             let ctx = Rc::clone(ctx);
             let screen = Rc::clone(&borrowed_ctx.screen);
 
-            Closure::new(move || {
+            Closure::new(move |ms: f64| {
+                const FRAME_DURATION_MS: f64 = 1000.0 / 59.737;
+
                 let mut borrowed_ctx = ctx.borrow_mut();
+                let max_frame_skip = borrowed_ctx.max_frame_skip;
+                let mut next_frame_ms = borrowed_ctx.next_frame_ms.unwrap_or(ms);
+
                 let Some(ref mut gba) = borrowed_ctx.gba else {
                     borrowed_ctx.updater = None;
                     return;
                 };
-
                 let mut screen = screen.borrow_mut();
-                while !take(&mut screen.new_frame) {
-                    gba.step(&mut *screen, &mut audio::NullCallback, false); // TODO: audio
+
+                let mut skipped_frames = 0;
+                loop {
+                    while !take(&mut screen.new_frame) {
+                        // TODO: audio
+                        gba.step(&mut *screen, &mut audio::NullCallback, skipped_frames > 0);
+                    }
+
+                    next_frame_ms += FRAME_DURATION_MS;
+                    if next_frame_ms > ms {
+                        borrowed_ctx.next_frame_ms = Some(next_frame_ms);
+                        break;
+                    }
+
+                    if skipped_frames >= max_frame_skip {
+                        // Too far behind; reschedule for the next frame.
+                        borrowed_ctx.next_frame_ms = None;
+                        break;
+                    }
+                    skipped_frames += 1;
                 }
 
-                // TODO: proper scheduling, frame skip
                 borrowed_ctx
                     .window
                     .request_animation_frame(
@@ -203,7 +228,6 @@ fn main() {
                 alert(&ctx.borrow().window, "Invalid BIOS ROM size!");
                 return;
             };
-
             ctx.borrow_mut().selected_bios_rom = Some(rom);
             maybe_start_emulation(&ctx);
         }
@@ -215,36 +239,63 @@ fn main() {
                 alert(&ctx.borrow().window, "Invalid cartridge ROM size!");
                 return;
             };
-
             ctx.borrow_mut().selected_cart_rom = Some(rom);
             maybe_start_emulation(&ctx);
         }
     });
 
     let document = window.document().unwrap();
-    document.set_onkeydown(Some(
-        create_keypress_handler(Rc::clone(&ctx), true)
+    document
+        .add_event_listener_with_callback(
+            "keydown",
+            create_keypress_handler(&ctx, true)
+                .into_js_value()
+                .unchecked_ref(),
+        )
+        .unwrap();
+    document
+        .add_event_listener_with_callback(
+            "keyup",
+            create_keypress_handler(&ctx, false)
+                .into_js_value()
+                .unchecked_ref(),
+        )
+        .unwrap();
+
+    let frame_skip_input = document
+        .get_element_by_id("memetendo-frame-skip")
+        .unwrap()
+        .dyn_into::<HtmlInputElement>()
+        .unwrap();
+    frame_skip_input.set_value(&ctx.borrow().max_frame_skip.to_string());
+    frame_skip_input
+        .add_event_listener_with_callback("change", {
+            let ctx = Rc::clone(&ctx);
+            Closure::<dyn Fn(_)>::new(move |event: Event| {
+                let input = event
+                    .target()
+                    .unwrap()
+                    .dyn_into::<HtmlInputElement>()
+                    .unwrap();
+                ctx.borrow_mut().max_frame_skip = input.value().parse().unwrap();
+            })
             .into_js_value()
-            .unchecked_ref(),
-    ));
-    document.set_onkeyup(Some(
-        create_keypress_handler(Rc::clone(&ctx), false)
-            .into_js_value()
-            .unchecked_ref(),
-    ));
+            .unchecked_ref()
+        })
+        .unwrap();
 }
 
 // TODO: uses event.code(), so we need to have some sort of prompt that shows the actual key if the
 // keyboard layout isn't QWERTY.
 fn create_keypress_handler(
-    ctx: Rc<RefCell<Context>>,
+    ctx: &Rc<RefCell<Context>>,
     pressed: bool,
 ) -> Closure<dyn FnMut(KeyboardEvent)> {
+    let ctx = Rc::clone(ctx);
     Closure::new(move |event: KeyboardEvent| {
         let Some(ref mut gba) = ctx.borrow_mut().gba else {
             return;
         };
-
         let key = match event.code().as_str() {
             "KeyX" => Key::A,
             "KeyZ" => Key::B,
@@ -264,30 +315,31 @@ fn create_keypress_handler(
 
 fn init_file_input(ctx: &Context, id: &str, mut callback: impl FnMut(Vec<u8>) + 'static) {
     let reader = FileReader::new().unwrap();
+    reader
+        .add_event_listener_with_callback("loadend", {
+            let window = ctx.window.clone();
+            Closure::<dyn FnMut(_)>::new(move |event: Event| {
+                let reader = event.target().unwrap().dyn_into::<FileReader>().unwrap();
+                let array_buf = reader.result().unwrap();
+                if array_buf.is_null() {
+                    let dom_exception = reader.error().unwrap();
+                    alert(
+                        &window,
+                        format!(
+                            "Failed to open file: {} (code {}).",
+                            dom_exception.message(),
+                            dom_exception.code(),
+                        ),
+                    );
+                    return;
+                }
 
-    reader.set_onloadend(Some({
-        let window = ctx.window.clone();
-        Closure::<dyn FnMut(_)>::new(move |event: Event| {
-            let reader = event.target().unwrap().dyn_into::<FileReader>().unwrap();
-            let array_buf = reader.result().unwrap();
-            if array_buf.is_null() {
-                let dom_exception = reader.error().unwrap();
-                alert(
-                    &window,
-                    format!(
-                        "Failed to open file: {} (code {}).",
-                        dom_exception.message(),
-                        dom_exception.code(),
-                    ),
-                );
-                return;
-            }
-
-            callback(Uint8Array::new(&array_buf).to_vec());
+                callback(Uint8Array::new(&array_buf).to_vec());
+            })
+            .into_js_value()
+            .unchecked_ref()
         })
-        .into_js_value()
-        .unchecked_ref()
-    }));
+        .unwrap();
 
     let input = ctx
         .document
@@ -296,7 +348,6 @@ fn init_file_input(ctx: &Context, id: &str, mut callback: impl FnMut(Vec<u8>) + 
         .dyn_into::<HtmlInputElement>()
         .unwrap();
     input.set_value("");
-
     input
         .add_event_listener_with_callback("change", {
             let window = ctx.window.clone();
@@ -312,7 +363,6 @@ fn init_file_input(ctx: &Context, id: &str, mut callback: impl FnMut(Vec<u8>) + 
                     alert(&window, "One file must be selected!");
                     return;
                 }
-
                 let file = files.item(0).unwrap();
                 reader.read_as_array_buffer(&file).unwrap();
             })
