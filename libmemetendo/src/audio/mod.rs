cd0c792f6d8188e8ad36e506c7b1bbffd8a85562
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2024-03-23 23:11:38 +0000

    perf(audio): sample mixing caching
    mix_sample is rather expensive. To avoid recomputing samples if conditions
    didn't change, introduce a cache for it.
    
    Perf reports a 7% runtime improvement in Audio::step for memetendo for Pokemon:
    Emerald's intro! (24.14% -> 17.24%)

--- libmemetendo/src/audio/mod.rs
+++ libmemetendo/src/audio/mod.rs
@@ -32,9 +32,42 @@ pub struct Audio {
     fifo_timer_idx: [usize; 2],
     bias: i16,
     sampling_cycle: u8,
+    mix_cache: cache::Mix,
 
     cached_soundcnt_bits: u64,
     cached_soundbias_bits: u64,
+}
+
+mod cache {
+    /// Cache for certain values computed by `mixed_sample`, to be potentially reused.
+    #[derive(Debug, Default)]
+    pub struct Mix {
+        dmg: Option<([u8; 4], (i16, i16))>,
+        fifo: Option<([i8; 2], (i16, i16))>,
+        pub mixed_sample: Option<(i16, i16)>,
+    }
+
+    impl Mix {
+        pub fn set_dmg(&mut self, volumes_sample: Option<([u8; 4], (i16, i16))>) {
+            self.dmg = volumes_sample;
+            self.mixed_sample = None;
+        }
+
+        #[must_use]
+        pub fn dmg(&self) -> Option<([u8; 4], (i16, i16))> {
+            self.dmg
+        }
+
+        pub fn set_fifo(&mut self, samples_sample: Option<([i8; 2], (i16, i16))>) {
+            self.fifo = samples_sample;
+            self.mixed_sample = None;
+        }
+
+        #[must_use]
+        pub fn fifo(&self) -> Option<([i8; 2], (i16, i16))> {
+            self.fifo
+        }
+    }
 }
 
 /// Right now, samples are outputted at the same rate that the frequency timer is emulated.
@@ -52,6 +85,8 @@ impl Audio {
     }
 
     pub fn reset(&mut self, skip_bios: bool) {
+        self.mix_cache = cache::Mix::default();
+
         // TODO: proper resetting; for now, just reset SOUNDBIAS so audio doesn't suck
         if skip_bios {
             self.write_word(0x88, 0x0000_0200);
@@ -109,62 +144,101 @@ impl Audio {
         }
     }
 
-    fn mix_sample(&self) -> (i16, i16) {
-        let mix_dmg = |out_channels: &[bool], out_volume| {
-            let mut sum = 0;
-            if out_channels[0] {
-                sum += i16::from(self.channels.0.volume());
-            }
-            if out_channels[1] {
-                sum += i16::from(self.channels.1.volume());
-            }
-            if out_channels[2] {
-                sum += i16::from(self.channels.2.volume());
-            }
-            if out_channels[3] {
-                sum += i16::from(self.channels.3.volume());
-            }
+    fn mix_sample(&mut self) -> (i16, i16) {
+        let dmg_volumes = [
+            self.channels.0.volume(),
+            self.channels.1.volume(),
+            self.channels.2.volume(),
+            self.channels.3.volume(),
+        ];
+        let fifo_samples = [self.channels.4.sample(), self.channels.5.sample()];
+
+        // Might need to invalidate the cache if the channel outputs changed.
+        if !self
+            .mix_cache
+            .dmg()
+            .is_some_and(|(volumes, _)| volumes == dmg_volumes)
+        {
+            self.mix_cache.set_dmg(None);
+        }
+        if !self
+            .mix_cache
+            .fifo()
+            .is_some_and(|(samples, _)| samples == fifo_samples)
+        {
+            self.mix_cache.set_fifo(None);
+        }
+        if let Some(mixed_sample) = self.mix_cache.mixed_sample {
+            return mixed_sample;
+        }
+
+        let mix_dmg = |out_channels: &[bool; 4], out_volume| {
+            let sum: i16 = dmg_volumes
+                .iter()
+                .zip(out_channels.iter())
+                .filter(|(_, &out)| out)
+                .map(|(&volume, _)| i16::from(volume))
+                .sum();
 
             (sum * i16::from(out_volume + 1)) >> (2 - self.dmg_volume_ratio)
         };
-        let mixed_dmg = (
-            mix_dmg(&self.out_channels.0, self.out_dmg_volume.0),
-            mix_dmg(&self.out_channels.1, self.out_dmg_volume.1),
-        );
-
-        let mix_fifo = |out_channels: &[bool]| {
-            let mut sum = 0;
-            if out_channels[4] {
-                sum += (4 * i16::from(self.channels.4.sample()))
-                    / if self.fifo_full_volume[0] { 1 } else { 2 };
-            }
-            if out_channels[5] {
-                sum += (4 * i16::from(self.channels.5.sample()))
-                    / if self.fifo_full_volume[1] { 1 } else { 2 };
-            }
-
-            sum
+        let mixed_dmg_sample = if let Some((_, sample)) = self.mix_cache.dmg() {
+            sample
+        } else {
+            let sample = (
+                mix_dmg(
+                    &self.out_channels.0[..4].try_into().unwrap(),
+                    self.out_dmg_volume.0,
+                ),
+                mix_dmg(
+                    &self.out_channels.1[..4].try_into().unwrap(),
+                    self.out_dmg_volume.1,
+                ),
+            );
+            self.mix_cache.set_dmg(Some((dmg_volumes, sample)));
+            sample
         };
-        let mixed_fifo = (
-            mix_fifo(&self.out_channels.0),
-            mix_fifo(&self.out_channels.1),
-        );
 
-        let mut sample = (mixed_dmg.0 + mixed_fifo.0, mixed_dmg.1 + mixed_fifo.1);
+        let mix_fifo = |out_channels: &[bool; 2]| -> i16 {
+            fifo_samples
+                .iter()
+                .zip(out_channels.iter().zip(self.fifo_full_volume.iter()))
+                .filter(|(_, (&out, _))| out)
+                .map(|(&sample, (_, &full_volume))| {
+                    (if full_volume { 4 } else { 2 }) * i16::from(sample)
+                })
+                .sum()
+        };
+        let mixed_fifo_sample = if let Some((_, sample)) = self.mix_cache.fifo() {
+            sample
+        } else {
+            let sample = (
+                mix_fifo(&self.out_channels.0[4..].try_into().unwrap()),
+                mix_fifo(&self.out_channels.1[4..].try_into().unwrap()),
+            );
+            self.mix_cache.set_fifo(Some((fifo_samples, sample)));
+            sample
+        };
+
+        let mut mixed_sample = (
+            mixed_dmg_sample.0 + mixed_fifo_sample.0,
+            mixed_dmg_sample.1 + mixed_fifo_sample.1,
+        );
         // Apply the bias, which is used to ensure the sample is clipped within the signed 10-bit
         // range for the DAC.
-        sample.0 += self.bias;
-        sample.1 += self.bias;
-        sample.0 = sample.0.clamp(0, 0x3ff);
-        sample.1 = sample.1.clamp(0, 0x3ff);
-        sample.0 -= self.bias;
-        sample.1 -= self.bias;
+        mixed_sample.0 += self.bias;
+        mixed_sample.0 = mixed_sample.0.clamp(0, 0x3ff);
+        mixed_sample.0 -= self.bias;
+        mixed_sample.1 += self.bias;
+        mixed_sample.1 = mixed_sample.1.clamp(0, 0x3ff);
+        mixed_sample.1 -= self.bias;
 
         // Scale to the i16 range.
-        sample.0 = sample.0.saturating_mul(i16::MAX / 0x200);
-        sample.1 = sample.1.saturating_mul(i16::MAX / 0x200);
+        mixed_sample.0 = mixed_sample.0.saturating_mul(i16::MAX / 0x200);
+        mixed_sample.1 = mixed_sample.1.saturating_mul(i16::MAX / 0x200);
 
-        sample
+        self.mix_cache.mixed_sample = Some(mixed_sample);
+        mixed_sample
     }
 
     pub fn notify_timer_overflow(&mut self, timer_idx: usize, count: u8) {
@@ -300,6 +374,8 @@ impl Bus for Audio {
                     0 => {
                         self.out_dmg_volume.0 = value.bits(..3);
                         self.out_dmg_volume.1 = value.bits(4..7);
+
+                        self.mix_cache.set_dmg(None);
                     }
                     1 => {
                         self.out_channels.0[0] = value.bit(0);
@@ -311,12 +387,17 @@ impl Bus for Audio {
                         self.out_channels.1[1] = value.bit(5);
                         self.out_channels.1[2] = value.bit(6);
                         self.out_channels.1[3] = value.bit(7);
+
+                        self.mix_cache.set_dmg(None);
                     }
                     // SOUNDCNT_H
                     2 => {
                         self.dmg_volume_ratio = value.bits(0..2).min(2);
                         self.fifo_full_volume[0] = value.bit(2);
                         self.fifo_full_volume[1] = value.bit(3);
+
+                        self.mix_cache.set_dmg(None);
+                        self.mix_cache.set_fifo(None);
                     }
                     3 => {
                         self.out_channels.1[4] = value.bit(0);
@@ -332,6 +413,8 @@ impl Bus for Audio {
                         if value.bit(7) {
                             self.channels.5.reset();
                         }
+
+                        self.mix_cache.set_fifo(None);
                     }
                     // SOUNDCNT_X
                     4 if replace(&mut self.enabled, value.bit(7)) && !self.enabled => {
@@ -355,6 +438,8 @@ impl Bus for Audio {
                     }
                     _ => {}
                 }
+
+                self.mix_cache.mixed_sample = None;
             }
             // WAVE_RAM
             0x90..=0x9f => self.channels.2.wave_ram().write_byte(addr & 0xf, value),
